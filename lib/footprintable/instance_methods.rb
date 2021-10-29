60d91f98c6f372c7afe4350964f6a103850d727b
sen <hdy9515@dingtalk.com>
2021-10-29 21:25:27 +0800

    FIX: destroy save full fields to before

--- lib/footprintable/instance_methods.rb
+++ lib/footprintable/instance_methods.rb
@@ -7,14 +7,18 @@ module Footprintable
       attrs_changed = attrs_changed.except *self.class.footprint_options[:except]
       attrs_changed = attrs_changed.extract! *self.class.footprint_options[:extract] if self.class.footprint_options[:extract].present?
 
-      attrs_before  = {}
-      attrs_after = {}
+      if action === :destroy
+        self.footprints.model.new(before: self, after: {}, action: action, trackable: self, actorable: Current.actor).save(validate: false)
+      else
+        attrs_before  = {}
+        attrs_after = {}
 
-      attrs_changed.each do |k, v|
-        attrs_before.store(k, v.first) unless v.first.nil?
-        attrs_after.store(k, v.last)  unless v.last.nil?
+        attrs_changed.each do |k, v|
+          attrs_before.store(k, v.first)# unless v.first.nil?
+          attrs_after.store(k, v.last)#  unless v.last.nil?
+        end
+        self.footprints.model.new(before: attrs_before, after: attrs_after, action: action, trackable: self, actorable: Current.actor).save(validate: false)
       end
-      self.footprints.model.new(before: attrs_before, after: attrs_after, action: action, trackable: self, actorable: Current.actor).save(validate: false)
     end
   end
 end
--- lib/footprintable/version.rb
+++ lib/footprintable/version.rb
@@ -1,3 +1,3 @@
 module Footprintable
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
 end
