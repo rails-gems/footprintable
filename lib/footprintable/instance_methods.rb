222c46dcec8bb9a13cf18c44e1faa9e9cfd78ae0
sen <hdy9515@dingtalk.com>
2021-10-30 23:13:51 +0800

    FIX: destroy footprint

--- lib/footprintable/instance_methods.rb
+++ lib/footprintable/instance_methods.rb
@@ -2,14 +2,14 @@ require 'footprintable/current'
 module Footprintable
   module InstanceMethods
     def create_footprint action
-      attrs_changed = previous_changes
-      return if attrs_changed.empty?
-      attrs_changed = attrs_changed.except *self.class.footprint_options[:except]
-      attrs_changed = attrs_changed.extract! *self.class.footprint_options[:extract] if self.class.footprint_options[:extract].present?
-
-      if action === :destroy
+      if action == :destroy
         self.footprints.model.new(before: self, after: {}, action: action, trackable: self, actorable: Current.actor).save(validate: false)
       else
+        attrs_changed = previous_changes
+        attrs_changed = attrs_changed.except *self.class.footprint_options[:except]
+        attrs_changed = attrs_changed.extract! *self.class.footprint_options[:extract] if self.class.footprint_options[:extract].present?
+        return if attrs_changed.empty?
+
         attrs_before  = {}
         attrs_after = {}
 
--- lib/footprintable/version.rb
+++ lib/footprintable/version.rb
@@ -1,3 +1,3 @@
 module Footprintable
-  VERSION = '0.1.4'
+  VERSION = '0.1.5'
 end
