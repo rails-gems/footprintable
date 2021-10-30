0a1f525b0faab01d65c5383634d4d5569543a9e9
sen <hdy9515@dingtalk.com>
2021-10-30 23:23:51 +0800

    FIX: remove nil attrs to store

--- lib/footprintable/instance_methods.rb
+++ lib/footprintable/instance_methods.rb
@@ -14,8 +14,8 @@ module Footprintable
         attrs_after = {}
 
         attrs_changed.each do |k, v|
-          attrs_before.store(k, v.first)# unless v.first.nil?
-          attrs_after.store(k, v.last)#  unless v.last.nil?
+          attrs_before.store(k, v.first) unless v.first.nil?
+          attrs_after.store(k, v.last) unless v.last.nil?
         end
         self.footprints.model.new(before: attrs_before, after: attrs_after, action: action, trackable: self, actorable: Current.actor).save(validate: false)
       end
--- lib/footprintable/version.rb
+++ lib/footprintable/version.rb
@@ -1,3 +1,3 @@
 module Footprintable
-  VERSION = '0.1.5'
+  VERSION = '0.1.6'
 end
